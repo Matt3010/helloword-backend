30ffd186618a12df57941c05aceca40a0c125110
Matteo Scanferla <m.scanferla@eggonemail.com>
2025-09-20 22:05:22 +0200

    refactor GameGateway for improved code style and add TODO for future enhancements

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -1,12 +1,12 @@
-import { WebSocketGateway, WebSocketServer, OnGatewayConnection } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import {OnGatewayConnection, WebSocketGateway, WebSocketServer} from '@nestjs/websockets';
+import {Server, Socket} from 'socket.io';
 
-@WebSocketGateway({ cors: { origin: '*' } })
+@WebSocketGateway({cors: {origin: '*'}})
 export class GameGateway implements OnGatewayConnection {
-  @WebSocketServer()
-  server!: Server;
+    @WebSocketServer()
+    server!: Server;
 
-  handleConnection(client: Socket) {
-    // placeholder. clients can join rooms if needed.
-  }
+    handleConnection(client: Socket) {
+        // TODO - For future, notify game change to user
+    }
 }
