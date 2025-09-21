b2f0851c0a2c2279a229d85e990bf4e487f4e0c6
Matteo Scanferla <m.scanferla@eggonemail.com>
2025-09-21 03:27:02 +0200

    enhance Docker support with multi-platform build and update Nginx configuration

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -12,7 +12,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         super({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:4000/api/auth/google/callback",
+            callbackURL: `${process.env.FE_DNS}/api/auth/google/callback`,
             scope: ['profile', 'email'],
         });
     }
